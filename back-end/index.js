bfb99475857f735227e45327e3d2baa6e88d4abb
Carlos Santos <carlosbenfica23@gmail.com>
2022-04-14 13:16:55 +0100

    Ashes view all data/ some css/null images replace for place hoder

--- back-end/index.js
+++ back-end/index.js
@@ -1,68 +1,67 @@
-const express = require('express');
-var request = require('request');
+const express = require("express");
+var request = require("request");
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-    res.send('root')
+app.get("/", (req, res) => {
+  res.send("root");
 });
 
-app.get('/clean', (req, res) => {
-    request(
-        'https://eldenring.fanapis.com/api/ashes',
-         function(error, response, body){
-             if (!error && response.statusCode == 200){
-                 res.send(body);
-             }
-        }
-    );
- });
+app.get("/clean", (req, res) => {
+  request(
+    "https://eldenring.fanapis.com/api/ashes",
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send(body);
+      }
+    }
+  );
+});
 
- app.get('/getashes', (req, res) => {
-    request(
-        'https://eldenring.fanapis.com/api/ashes',
-         function(error, response, body){
-             //console.log(body);
-             if (!error && response.statusCode == 200){
-                 var parsedBody = JSON.parse(body);
-                 var data = parsedBody ["data"];
-                 res.send({data});
-             }
-        }
-    );
- });
+app.get("/getashes", (req, res) => {
+  request(
+    "https://eldenring.fanapis.com/api/ashes",
+    function (error, response, body) {
+      //console.log(body);
+      if (!error && response.statusCode == 200) {
+        var parsedBody = JSON.parse(body);
+        var data = parsedBody["data"];
+        res.json(data);
+      }
+    }
+  );
+});
 
- //only affinities
- app.get('/getashesaffinity', (req, res) => {
-    request(
-        'https://eldenring.fanapis.com/api/ashes',
-         function(error, response, body){
-             if (!error && response.statusCode == 200){
-                 var parsedBody = JSON.parse(body);
-                 var affinity = [];
-                 for(i = 0 ; i < parsedBody.data.length; i++){
-                    affinity[i] = parsedBody.data[i].affinity;
-                    //console.log(i);
-                 }
-                //var affinity = parsedBody.data[0].affinity;
-                res.send({affinity});
-                 //var data = parsedBody ["data"];       
-             }
+//only affinities
+app.get("/getashesaffinity", (req, res) => {
+  request(
+    "https://eldenring.fanapis.com/api/ashes",
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var parsedBody = JSON.parse(body);
+        var affinity = [];
+        for (i = 0; i < parsedBody.data.length; i++) {
+          affinity[i] = parsedBody.data[i].affinity;
+          //console.log(i);
         }
-    );
- });
-
+        //var affinity = parsedBody.data[0].affinity;
+        res.send({ affinity });
+        //var data = parsedBody ["data"];
+      }
+    }
+  );
+});
 
 // POST method route
-app.post('/', (req, res) => {
-    res.send('POST request to the homepage')
+app.post("/", (req, res) => {
+  res.send("POST request to the homepage");
 });
 
-app.all('/secret', (req, res, next) => {
-    console.log('Accessing the secret section ...')
-    next() // pass control to the next handler
+app.all("/secret", (req, res, next) => {
+  console.log("Accessing the secret section ...");
+  next(); // pass control to the next handler
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
--- front-end/src/ashes.js
+++ front-end/src/ashes.js
@@ -1,51 +1,50 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-
+import "./ashes.css";
 
 const Ashes = () => {
+  // const [name, setName] = useState('teste');
 
-    // const [name, setName] = useState('teste');
+  // const handClick = () => {
+  //    setName('Pass');
+  // }
 
-    // const handClick = () => {
-    //    setName('Pass');
-    // }
+  const url = "/getashes";
+  const [ashes, setAshes] = useState([]);
 
-   
-      const url = '/getashes';
-      const [ashes, setAshes] = useState([]);
-      
-    
-      useEffect (() => {
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      //console.log(response);
+      setAshes(response.data);
+    });
+  }, []);
 
-          axios.get(url).then(response => {
-            //console.log(response);
-            setAshes(response.data.data);
-          })
+  console.log(ashes);
 
-      }, [])
-    
-    console.log(ashes);
-    
-    if(ashes.length > 0){
-      return (
-      <>
-        <div>
-              <p>{ashes[0].name}</p>
-              <img src={ashes[0].image}/>
-              <p>{ashes[0].affinity}</p>
-              <p>{ashes[0].description}</p>
-              <p>{ashes[0].skill}</p>
-              
-        </div>
-      </>
-      );
-    }
+  if (ashes.length > 0) {
     return (
-      <>
-        <div>
-              <p>oi</p>
-        </div>
-      </>
+      <div className="ashecontainer">
+        {ashes?.map((ashe) => (
+          <div className="box1">
+            <p>{ashe.name}</p>
+            {ashe.image === null ? (
+              <img src={"https://via.placeholder.com/200"} />
+            ) : (
+              <img src={ashe.image} />
+            )}
+            <p>{ashe.affinity}</p>
+            {console.log(ashe.name)}
+          </div>
+        ))}
+      </div>
     );
-} 
+  }
+  return (
+    <>
+      <div>
+        <p>Loading</p>
+      </div>
+    </>
+  );
+};
 export default Ashes;
