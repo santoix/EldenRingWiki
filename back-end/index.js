8258b008bdb0fcb779ee9e2da35a3f838d315d9d
Carlos Santos <carlosbenfica23@gmail.com>
2022-04-14 07:19:08 +0100

    back-end add some gets and testing with postman

--- back-end/index.js
+++ back-end/index.js
@@ -1,14 +1,56 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+var request = require('request');
+const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
     res.send('root')
 });
 
-app.get('/main', (req, res) => {
-    res.send('main')
-});
+app.get('/clean', (req, res) => {
+    request(
+        'https://eldenring.fanapis.com/api/ashes',
+         function(error, response, body){
+             if (!error && response.statusCode == 200){
+                 res.send(body);
+             }
+        }
+    );
+ });
+
+ app.get('/getashes', (req, res) => {
+    request(
+        'https://eldenring.fanapis.com/api/ashes',
+         function(error, response, body){
+             if (!error && response.statusCode == 200){
+                 var parsedBody = JSON.parse(body);
+                 var data = parsedBody ["data"];
+                 res.send({data})
+             }
+        }
+    );
+ });
+
+ //only affinities
+ app.get('/getashesaffinity', (req, res) => {
+    request(
+        'https://eldenring.fanapis.com/api/ashes',
+         function(error, response, body){
+             if (!error && response.statusCode == 200){
+                 var parsedBody = JSON.parse(body);
+                 var affinity = [];
+                 for(i = 0 ; i < parsedBody.data.length; i++){
+                    affinity[i] = parsedBody.data[i].affinity;
+                    console.log(i);
+                 }
+                //var affinity = parsedBody.data[0].affinity;
+                res.send({affinity});
+                 //var data = parsedBody ["data"];       
+             }
+        }
+    );
+ });
+
 
 // POST method route
 app.post('/', (req, res) => {
